c0120167c7a4a384175935b3a46fb6ac3939ad1c
CZpets <simunekpetr98@gmail.com>
2021-08-19 22:00:35 +0200

    build_dummy warnings removed

--- bindings/build_dummy.cmd
+++ bindings/build_dummy.cmd
@@ -3,4 +3,4 @@ IF NOT exist noarr-structures (
     git clone https://github.com/ParaCoToUl/noarr-structures.git
 )
 
-cl /O2 /LD /I noarr-structures\include\ /I ..\include\ matrix_multiply_dummy.cpp /link /out:matrix_multiply.dll
+cl /O2 /LD /EHsc /I noarr-structures\include\ /I ..\include\ matrix_multiply_dummy.cpp /link /out:matrix_multiply.dll
