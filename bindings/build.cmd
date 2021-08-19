5fccaff5cb825969f03d17408adb5ef3b833865b
jiriklepl <jiriklepl@seznam.cz>
2021-08-19 04:48:26 +0200

    fixed include paths in build scripts

--- bindings/build.cmd
+++ bindings/build.cmd
@@ -3,4 +3,4 @@ IF NOT exist noarr-structures (
     git clone https://github.com/ParaCoToUl/noarr-structures.git
 )
 
-nvcc -O3 -arch=sm_35 -G --expt-relaxed-constexpr -I "noarr-structures\\include\\" -I "..\\..\\include\\" --shared -o matrix_multiply.dll matrix_multiply.cu
+nvcc -O3 -arch=sm_35 -G --expt-relaxed-constexpr -I "noarr-structures\\include\\" -I "..\\include\\" --shared -o matrix_multiply.dll matrix_multiply.cu
--- bindings/build.sh
+++ bindings/build.sh
@@ -3,5 +3,5 @@
 [ -d "noarr-structures" ] || git clone https://github.com/ParaCoToUl/noarr-structures.git
 
 nvcc -O3 -arch=sm_35 -G --expt-relaxed-constexpr \
-         -I "../../include/" -I "noarr-structures/include/" \
+         -I "../include/" -I "noarr-structures/include/" \
          --shared -Xcompiler -fPIC -o matrix_multiply.so matrix_multiply.cu
