d1481992ab87bc29e99a266ce9952ae59d5e5c1d
jiriklepl <jiriklepl@seznam.cz>
2021-08-19 03:26:40 +0200

    R bindings moved up; added python bindings; original separate folders deleted

---
+++ bindings/matrix_multiply.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from ctypes import *
+
+matrix_multiply = CDLL('./matrix_multiply.so')
+
+def matrix_multiply_demo(file, size, repetitions):
+    n_matrices = (c_int * 1)(repetitions)
+    matrices = (c_char_p * repetitions)()
+    heights = (c_int * repetitions)()
+    widths = (c_int * repetitions)()
+
+    for i in range(repetitions):
+        matrices[i] = bytes(file, encoding='utf8')
+        heights[i] = size
+        widths[i] = size
+
+    matrix_multiply.matrix_multiply_demo(n_matrices, matrices, heights, widths)
+
+matrix_multiply_demo('data/matrix32.data', 32, 32)
+matrix_multiply_demo('data/matrix64.data', 64, 64)
+matrix_multiply_demo('data/matrix96.data', 96, 96)
