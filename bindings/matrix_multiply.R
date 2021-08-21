e4a467b671455b26fdfbccbca647286ba729a0f2
jiriklepl <jiriklepl@seznam.cz>
2021-08-22 00:45:52 +0200

    added comments to the python and R codes

--- bindings/matrix_multiply.R
+++ bindings/matrix_multiply.R
@@ -1,6 +1,8 @@
 #!/usr/bin/env Rscript
 
 matrix_multiply_demo <- function(file, size, repetitions) {
+    # we test what platform the code runs on and then
+    # we load the dynamic library with the corresponding file extension
     if(!is.loaded("matrix_multiply_demo")) {
         if (Sys.info()["sysname"] == "Linux") {
             dyn.load("matrix_multiply.so")
@@ -11,10 +13,12 @@ matrix_multiply_demo <- function(file, size, repetitions) {
 
     n_matrices <- repetitions
 
-    matrices <- rep(file, n_matrices)
-    heights <- rep(size, n_matrices)
-    widths <- rep(size, n_matrices)
+    # here we create 3 vectors of the length `repetitions`
+    matrices <- rep(file, repetitions)
+    heights <- rep(size, repetitions)
+    widths <- rep(size, repetitions)
 
+    # we pass the 4 arguments to the C code, function `matrix_multiply_demo`
     res <- .C("matrix_multiply_demo",
         n_matrices=as.integer(n_matrices),
         matrices=as.character(matrices),
--- bindings/matrix_multiply.py
+++ bindings/matrix_multiply.py
@@ -3,22 +3,27 @@
 from ctypes import *
 import platform
 
+# we test what platform the code runs on and then
+# we load the dynamic library with the corresponding file extension
 if platform.system() == "Linux":
     matrix_multiply = CDLL('./matrix_multiply.so')
 elif platform.system() == "Windows":
     matrix_multiply = CDLL('.\matrix_multiply.dll')
 
 def matrix_multiply_demo(file, size, repetitions):
+    # this syntax creates 4 arrays (3 integer arrays and 1 c-string array)
     n_matrices = (c_int * 1)(repetitions)
     matrices = (c_char_p * repetitions)()
     heights = (c_int * repetitions)()
     widths = (c_int * repetitions)()
 
+    # here we initialize the arrays
     for i in range(repetitions):
         matrices[i] = bytes(file, encoding='utf8')
         heights[i] = size
         widths[i] = size
 
+    # we pass the 4 arrays to the C code, function `matrix_multiply_demo`
     matrix_multiply.matrix_multiply_demo(n_matrices, matrices, heights, widths)
 
 matrix_multiply_demo('data/matrix32.data', 32, 32)
